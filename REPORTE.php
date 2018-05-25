2be8e0eb2a9e21ab73304c01a432b298cdeecb84
CristianLRS1997 <clramirez576@misena.edu.co>
2018-05-25 17:37:35 -0500

    FFFF
    FFF

--- REPORTE.php
+++ REPORTE.php
@@ -5,30 +5,35 @@
 	include 'plantilla.php';
 	require 'conexion.php';
 
-	$query = "SELECT * FROM tbl_order ORDER BY order_id desc";
-	$resultado = $mysqli->query($query);
 
-	$pdf = new PDF();
-	$pdf->AliasNbPages();
-	$pdf->AddPage('L');
 
-	$pdf->SetFillColor(232,232,232);
-	$pdf->SetFont('Arial','B',12);
-	$pdf->Cell(10,6,'ID',1,0,'C',1);
-	$pdf->Cell(50,6,'Customer Name',1,0,'C',1);
-	$pdf->Cell(90,6,'Item',1,0,'C',1);
-	$pdf->Cell(40,6,'Value',1,0,'C',1);
-	$pdf->Cell(40,6,'Order Date',1,1,'C',1);
+    $query = "SELECT * FROM tbl_order WHERE order_date BETWEEN '" . $_POST["start"] . "' AND '" . $_POST["end"] . "' ";
+    $resultado = $mysqli->query($query);
 
-	$pdf->SetFont('Arial','',10);
+    $pdf = new PDF();
+    $pdf->AliasNbPages();
+    $pdf->AddPage('L');
 
-	while($row = $resultado->fetch_assoc())
-	{
-		$pdf->Cell(10,6,utf8_decode($row['order_id']),1,0,'C');
-		$pdf->Cell(50,6,$row['order_customer_name'],1,0,'C');
-		$pdf->Cell(90,6,utf8_decode($row['order_item']),1,0,'C');
-		$pdf->Cell(40,6,utf8_decode($row['order_value']),1,0,'C');
-		$pdf->Cell(40,6,utf8_decode($row['order_date']),1,1,'C');
-	}
-	$pdf->Output();
+    $pdf->SetFillColor(232, 232, 232);
+    $pdf->SetFont('Arial', 'B', 12);
+    $pdf->Cell(10, 6, 'ID', 1, 0, 'C', 1);
+    $pdf->Cell(50, 6, 'Customer Name', 1, 0, 'C', 1);
+    $pdf->Cell(90, 6, 'Item', 1, 0, 'C', 1);
+    $pdf->Cell(40, 6, 'Value', 1, 0, 'C', 1);
+    $pdf->Cell(40, 6, 'Order Date', 1, 1, 'C', 1);
+
+    $pdf->SetFont('Arial', '', 10);
+
+
+        while ($row = $resultado->fetch_assoc()) {
+            $pdf->Cell(10, 6, utf8_decode($row['order_id']), 1, 0, 'C');
+            $pdf->Cell(50, 6, $row['order_customer_name'], 1, 0, 'C');
+            $pdf->Cell(90, 6, utf8_decode($row['order_item']), 1, 0, 'C');
+            $pdf->Cell(40, 6, utf8_decode($row['order_value']), 1, 0, 'C');
+            $pdf->Cell(40, 6, utf8_decode($row['order_date']), 1, 1, 'C');
+        }
+        $pdf->Output();
+
+
+
 ?>
--- index.php
+++ index.php
@@ -20,73 +20,51 @@ $result = $mysqli->query($query);
 <div class="container" style="width:900px;">
     <h2 align="center">Ajax PHP MySQL Date Range Search using jQuery DatePicker</h2>
     <h3 align="center">Order Data</h3><br />
-    <div class="col-md-3">
-        <input type="text" name="from_date" id="from_date" class="form-control" placeholder="From Date" />
-    </div>
-    <div class="col-md-3">
-        <input type="text" name="to_date" id="to_date" class="form-control" placeholder="To Date" />
-    </div>
-    <div class="col-md-5">
-        <input type="button" name="filter" id="filter" value="Filter" class="btn btn-info" />
-    </div>
-    <div style="clear:both"></div>
-    <br />
-    <div id="order_table">
-        <table class="table table-bordered">
-            <tr>
-                <th width="5%">ID</th>
-                <th width="30%">Customer Name</th>
-                <th width="43%">Item</th>
-                <th width="10%">Value</th>
-                <th width="12%">Order Date</th>
-            </tr>
-            <?php
-            while($row = mysqli_fetch_array($result))
-            {
-                ?>
+
+
+    <form name="formulario" method="post" action="REPORTE.php">
+
+        <div class="col-md-3">
+            <input type="text" name="start" id="start" class="form-control" placeholder="From Date" />
+        </div>
+        <div class="col-md-3">
+            <input type="text" name="end" id="end" class="form-control" placeholder="To Date" />
+        </div>
+        <div class="col-md-5">
+            <input type="submit" name="filter" id="filter" value="Filter" class="btn btn-info" />
+        </div>
+        <div style="clear:both"></div>
+        <br />
+        <div id="order_table">
+            <table class="table table-bordered">
                 <tr>
-                    <td><?php echo $row["order_id"]; ?></td>
-                    <td><?php echo $row["order_customer_name"]; ?></td>
-                    <td><?php echo $row["order_item"]; ?></td>
-                    <td>$ <?php echo $row["order_value"]; ?></td>
-                    <td><?php echo $row["order_date"]; ?></td>
+                    <th width="5%">ID</th>
+                    <th width="30%">Customer Name</th>
+                    <th width="43%">Item</th>
+                    <th width="10%">Value</th>
+                    <th width="12%">Order Date</th>
                 </tr>
                 <?php
-            }
-            ?>
-        </table>
-    </div>
+                while($row = mysqli_fetch_array($result))
+                {
+                    ?>
+                    <tr>
+                        <td><?php echo $row["order_id"]; ?></td>
+                        <td><?php echo $row["order_customer_name"]; ?></td>
+                        <td><?php echo $row["order_item"]; ?></td>
+                        <td>$ <?php echo $row["order_value"]; ?></td>
+                        <td><?php echo $row["order_date"]; ?></td>
+                    </tr>
+                    <?php
+                }
+                ?>
+            </table>
+        </div>
+
+    </form>
+
+
 </div>
 </body>
 </html>
-<script>
-    $(document).ready(function(){
-        $.datepicker.setDefaults({
-            dateFormat: 'yy-mm-dd'
-        });
-        $(function(){
-            $("#from_date").datepicker();
-            $("#to_date").datepicker();
-        });
-        $('#filter').click(function(){
-            var from_date = $('#from_date').val();
-            var to_date = $('#to_date').val();
-            if(from_date != '' && to_date != '')
-            {
-                $.ajax({
-                    url:"REPORTE.php",
-                    method:"POST",
-                    data:{from_date:from_date, to_date:to_date},
-                    success:function(data)
-                    {
-                        window.location.href = "REPORTE.php";
-                    }
-                });
-            }
-            else
-            {
-                alert("Please Select Date");
-            }
-        });
-    });
-</script>
+
